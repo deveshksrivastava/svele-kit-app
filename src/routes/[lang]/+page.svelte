4ced08a2b07010d2a46786b3a5143cbdab3cffca
Devesh <devesh.kr.sri@gmail.com>
2023-07-13 17:04:52 +0100

    SvelteKit Tutorial - 9 - Optional Parameters from url for language

---
+++ src/routes/[lang]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import {page} from '$app/stores'
+  const greetings = {
+    en: 'Hello, world',
+    es: "Hola!",
+    fr: "Bonjour"
+  }
+
+  const {lang} = $page.params;
+  const greeting = greetings[lang]
+</script>
+
+<h1>{greeting} User!</h1>
+<!-- http://localhost:5173/fr -->
+<!-- http://localhost:5173/en -->
